228fd77ac41f4068c8f4779a3123948f0af80dc0
sadebare <adebaresulaimon04@gmail.com>
2022-09-16 18:06:39 +0100

    Configuration management:: Unstall flask

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,5 @@
 # Using Puppet, install flask from pip3.
-package { 'puppet-lint':
-  ensure   => '2.1.1',
-  name     => 'puppet-lint',
-  provider => 'gem'
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3'
 }
